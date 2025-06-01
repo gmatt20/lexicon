b285be6bd55228d6c50dcf389be79c2db3e44ee2
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-31 17:47:54 -0700

    separated into components

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,32 +1,15 @@
 import { Input } from "@/components/ui/input";
-import {
-  Card,
-  CardContent,
-} from "@/components/ui/card";
-
+import ChatBubble from "@/components/ChatBubble";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen">
-      <div className="p-5">
-        <div className="flex flex-col justify-between">
-          <div className="flex justify-start">
-            <Card className="bg-primary text-white w-fit">
-              <CardContent>
-                <p>Hello there, I am Lex</p>
-              </CardContent>
-            </Card>
-          </div>
-          <div className="flex justify-end">
-            <Card className="bg-primary-foreground text-black w-fit">
-              <CardContent>
-                <p>Hello Lex, I am Matthew!</p>
-              </CardContent>
-            </Card>
-          </div>
-        </div>
+      <div className="p-5 pb-32">
+        {[...Array(20)].map((_, i) => (
+          <ChatBubble key={i} />
+        ))}
       </div>
-      <div className="fixed bottom-20 w-full">
+      <div className="fixed bottom-0 w-full bg-white p-5">
         <Input type="email" placeholder="Type your message..." />
       </div>
     </div>
