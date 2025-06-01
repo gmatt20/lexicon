731511cf4468a917f5ae5cd056672ec6ec15717d
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-31 18:19:09 -0700

    imported Lexicon profile pic

--- client/components/ChatBubble.tsx
+++ client/components/ChatBubble.tsx
@@ -2,22 +2,20 @@ import { Card, CardContent } from "@/components/ui/card";
 
 export default function ChatBubble() {
   return (
-    <div className="p-5">
-      <div className="flex flex-col justify-between">
-        <div className="flex justify-start">
-          <Card className="bg-primary text-white w-fit">
-            <CardContent>
-              <p>Hello there, I am Lex</p>
-            </CardContent>
-          </Card>
-        </div>
-        <div className="flex justify-end">
-          <Card className="bg-primary-foreground text-black w-fit">
-            <CardContent>
-              <p>Hello Lex, I am Matthew!</p>
-            </CardContent>
-          </Card>
-        </div>
+    <div className="flex flex-col justify-between mx-10">
+      <div className="flex justify-start my-5">
+        <Card className="bg-primary text-white w-fit">
+          <CardContent>
+            <p>Hello there, I am Lex</p>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="flex justify-end my-5">
+        <Card className="bg-primary-foreground text-black w-fit">
+          <CardContent>
+            <p>Hello Lex, I am Matthew!</p>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
