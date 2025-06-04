c50b6b72b660ef105ecfddf69143f48e84477bc9
Matthew Garcia <gmatthew2765@gmail.com>
2025-06-03 22:29:35 -0700

    adding model comments

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,5 +1,7 @@
 from sqlmodel import Field, SQLModel, Relationship
 
+# TURD: Need to add time created, Google ID from oauth, email, password
+# TURD: and profile picture
 class User(SQLModel, table=True):
   # Marks the user ID as required in the database
   # and the database will generate a unique ID
@@ -8,6 +10,7 @@ class User(SQLModel, table=True):
   messages: list["Message"] = Relationship(back_populates="user")
   conversations: list["Conversation"] = Relationship(back_populates="user")
 
+# TURD: Need to add time created
 class Conversation(SQLModel, table=True):
   id: int | None = Field(default=None, primary_key=True)
   user_id: int | None = Field(foreign_key="user.id")
@@ -15,6 +18,7 @@ class Conversation(SQLModel, table=True):
   user: User | None = Relationship(back_populates="conversations")
   messages: list["Message"] = Relationship(back_populates="conversation")
 
+# TURD: Need to add time created
 class Message(SQLModel, table=True):
   id: int | None = Field(default=None, primary_key=True)
   user_id: int | None = Field(foreign_key="user.id")
