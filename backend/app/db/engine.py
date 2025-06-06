00538d7fe01d3c724294400eaca7e75520fddc2d
Matthew Garcia <gmatthew2765@gmail.com>
2025-06-05 20:21:12 -0700

    integrated supabase DB

--- backend/app/db/engine.py
+++ backend/app/db/engine.py
@@ -3,11 +3,28 @@ from fastapi import Depends
 from typing import Annotated
 from dotenv import load_dotenv
 import os
+from supabase import create_client, Client
 
 load_dotenv()
 
-psql_url = os.getenv("PSQL_URL")
-engine = create_engine(psql_url,)
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+engine = create_engine(DATABASE_URL)
+
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Failed to connect: {e}")
 
 def create_db_and_tables():
   # SQLModel.metadata.drop_all(engine)
