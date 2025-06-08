264969b362a0eb08f38701f8853401ce1f6a7711
Matthew Garcia <gmatthew2765@gmail.com>
2025-06-07 17:05:52 -0700

    simple landing page

--- client/components/landing.tsx
+++ client/components/landing.tsx
@@ -1,7 +1,17 @@
-export default function Landing(){
-  return(
-    <div>
+import Lexicon from "@/public/lexicon.webp";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+export default function Landing() {
+  return (
+    <div className="w-screen flex items-center justify-center flex-col">
+      <Image
+        className="relative flex size-[30%] shrink-0 overflow-hidden rounded-3xl"
+        src={Lexicon}
+        alt="Lexicon"
+      />
       <p>introducing lex!</p>
+      <Button>Chat for free</Button>
     </div>
   );
 }
