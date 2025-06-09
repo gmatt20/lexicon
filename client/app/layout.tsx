eced945187aaf8970979206d4ac5f84a8e57dfc9
Matthew Garcia <gmatthew2765@gmail.com>
2025-06-08 21:37:05 -0700

    global navbar

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import Navbar from "@/components/navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <main>{children}</main>
         <Toaster />
       </body>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,8 @@
 import Landing from "@/components/landing";
-import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <main className="mt-30">
         <Landing />
       </main>
