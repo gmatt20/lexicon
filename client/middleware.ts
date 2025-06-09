936188d820c8998474608f89959b4ca33a47eced
Matthew Garcia <gmatthew2765@gmail.com>
2025-06-08 21:04:35 -0700

    implemented middleware

---
+++ client/middleware.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("access_token")?.value;
+
+  if (request.nextUrl.pathname.startsWith("/chat")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+  }
+  return NextResponse.next();
+}
