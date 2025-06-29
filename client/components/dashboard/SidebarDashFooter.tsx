2454956d441473cb42ea3af356433395ca378035
Matthew Garcia <gmatthew2765@gmail.com>
2025-06-28 21:00:04 -0700

    refactor: made sidebar dash footer less atrocious

---
+++ client/components/dashboard/SidebarDashFooter.tsx
@@ -0,0 +1,81 @@
+import { SidebarMenuItem, SidebarMenuButton } from "../ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { User2, ChevronUp } from "lucide-react";
+import { useAuthentication } from "@/hooks/useAuthentication";
+import { useConversations } from "@/hooks/useConversations";
+import { useRouter } from "next/navigation";
+
+const SidebarDashFooter = () => {
+  const router = useRouter();
+  const { user, signOut } = useAuthentication();
+  const { deleteConvos } = useConversations();
+  const handleSettings = () => {
+    router.push("/settings");
+  };
+
+  return (
+    <SidebarMenuItem>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <SidebarMenuButton>
+            <User2 /> {user && user.username}
+            <ChevronUp className="ml-auto" />
+          </SidebarMenuButton>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          side="top"
+          className="w-[--radix-popper-anchor-width]">
+          <Dialog>
+            <DialogTrigger asChild>
+              <span className="hover:bg-accent focus:text-accent-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none">
+                Delete all conversations
+              </span>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Delete all conversations?</DialogTitle>
+                <DialogDescription>
+                  This action cannot be undone.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button variant="outline">Cancel</Button>
+                </DialogClose>
+                <DialogClose asChild>
+                  <Button variant="destructive" onClick={deleteConvos}>
+                    Delete
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+          <DropdownMenuItem onClick={handleSettings}>
+            <span>Account</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={signOut}>
+            <span>Sign out</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </SidebarMenuItem>
+  );
+};
+
+export default SidebarDashFooter;
--- client/components/dashboard/dash-sidebar.tsx
+++ client/components/dashboard/dash-sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Home, SquarePen, User2, ChevronUp } from "lucide-react";
+import { Home, SquarePen } from "lucide-react";
 
 import {
   Sidebar,
@@ -9,61 +9,21 @@ import {
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
   SidebarFooter,
 } from "@/components/ui/sidebar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useAuthentication } from "@/hooks/useAuthentication";
 import { useConversations } from "@/hooks/useConversations";
 import { useRouter } from "next/navigation";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
 import ChatsList from "./ChatsList";
 import SidebarItem from "./SidebarItem";
+import SidebarDashFooter from "@/components/dashboard/SidebarDashFooter";
 
 export function DashSidebar() {
   const router = useRouter();
-  const { user, signOut } = useAuthentication();
-  const {
-    convos,
-    loading,
-    newConvo,
-    renameConvoById,
-    deleteConvoById,
-    deleteConvos,
-  } = useConversations();
+
+  const { newConvo } = useConversations();
 
   const handleHome = async () => {
     router.push("/dashboard");
-  };
-  const handleSettings = () => {
-    router.push("/settings");
-  };
-  const FormAction = async (
-    e: React.FormEvent<HTMLFormElement>,
-    convoId: number,
-  ) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    console.log(formData);
-    const name = formData.get("name") as string;
-    console.log(name);
-    renameConvoById(convoId, name);
   };
 
   return (
@@ -86,52 +46,7 @@ export function DashSidebar() {
       </SidebarContent>
       <SidebarFooter>
         <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton>
-                  <User2 /> {user && user.username}
-                  <ChevronUp className="ml-auto" />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                side="top"
-                className="w-[--radix-popper-anchor-width]"
-              >
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <span className="hover:bg-accent focus:text-accent-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none">
-                      Delete all conversations
-                    </span>
-                  </DialogTrigger>
-                  <DialogContent>
-                    <DialogHeader>
-                      <DialogTitle>Delete all conversations?</DialogTitle>
-                      <DialogDescription>
-                        This action cannot be undone.
-                      </DialogDescription>
-                    </DialogHeader>
-                    <DialogFooter>
-                      <DialogClose asChild>
-                        <Button variant="outline">Cancel</Button>
-                      </DialogClose>
-                      <DialogClose asChild>
-                        <Button variant="destructive" onClick={deleteConvos}>
-                          Delete
-                        </Button>
-                      </DialogClose>
-                    </DialogFooter>
-                  </DialogContent>
-                </Dialog>
-                <DropdownMenuItem onClick={handleSettings}>
-                  <span>Account</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={signOut}>
-                  <span>Sign out</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
+          <SidebarDashFooter />
         </SidebarMenu>
       </SidebarFooter>
     </Sidebar>
