3b81a6f9948e1126b537fb8e0832a4719d21d4e9
Matthew Garcia <gmatthew2765@gmail.com>
2025-07-02 16:28:50 -0700

    refactir: moved security settings component with account deletion functionality

--- client/app/settings/security/page.tsx
+++ client/app/settings/security/page.tsx
@@ -1,50 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { useAuthentication } from "@/hooks/useAuthentication";
-
 export default function Security() {
-  const { user, deleteAccount } = useAuthentication();
-
-  console.log(user);
-
-  return (
-    <div className="mx-3">
-      <p className="text-3xl font-montserrat font-bold mb-5">security</p>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="destructive">Delete Account</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Delete your account?</DialogTitle>
-            <DialogDescription>
-              All of your conversations, messages, and account information will
-              be deleted. This action cannot be undone.
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <DialogClose asChild>
-              <Button onClick={deleteAccount} variant="destructive">
-                Delete Account
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
+  return <div className="w-full"></div>;
 }
---
+++ client/components/settings/security/security.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useAuthentication } from "@/hooks/useAuthentication";
+
+const Security = () => {
+  const { deleteAccount } = useAuthentication();
+
+  return (
+    <div className="mx-3">
+      <p className="text-3xl font-montserrat font-bold mb-5">security</p>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="destructive">Delete Account</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete your account?</DialogTitle>
+            <DialogDescription>
+              All of your conversations, messages, and account information will
+              be deleted. This action cannot be undone.
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <DialogClose asChild>
+              <Button onClick={deleteAccount} variant="destructive">
+                Delete Account
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default Security;
