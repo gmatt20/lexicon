01ec9cca6ca04519bbb3bd5b3f3857a32ce570bd
Matthew Garcia <gmatthew2765@gmail.com>
2025-07-12 18:25:48 -0700

    refactor: add missing access token check in verify_token function and update tests for consistency

--- backend/app/services/verify_token.py
+++ backend/app/services/verify_token.py
@@ -11,6 +11,11 @@ SUPABASE_JWT_SECRET = os.getenv("SUPABASE_JWT_SECRET")
 
 def verify_token(request: Request):
     token = request.cookies.get("access_token")
+    if token is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Access token is missing",
+        )
     try:
         payload = jwt.decode(
             token, SUPABASE_JWT_SECRET, algorithms=[ALGORITHM], audience="authenticated"
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -8,3 +8,8 @@ def test_read_main():
   response = client.get("/")
   assert response.status_code == 200
   assert response.json() == {"message": "Welcome to the Lexicon App!"}
+
+def test_get_conversations():
+  response = client.get("/conversations/")
+  assert response.status_code == 401
+  assert response.json() == {"detail": "Access token is missing"}
