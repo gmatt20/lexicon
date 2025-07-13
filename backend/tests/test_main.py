3a0aabdca4cc6a08f3ee9451c51b758677c9f9e2
Matthew Garcia <gmatthew2765@gmail.com>
2025-07-12 18:37:06 -0700

    test: testing endpoints for unauthenticated access checks

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -4,12 +4,26 @@ from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
+# Preliminary test for main API endpoint
 def test_read_main():
   response = client.get("/")
   assert response.status_code == 200
   assert response.json() == {"message": "Welcome to the Lexicon App!"}
 
-def test_get_conversations():
-  response = client.get("/conversations/")
-  assert response.status_code == 401
-  assert response.json() == {"detail": "Access token is missing"}
+# Tests get conversations for the unauthenticated
+def test_get_conversations_unauth():
+    response = client.get("/conversations/")
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Access token is missing"}
+
+# Tests get message by ID for the unauthenticated
+def test_get_messages_unauth():
+    response = client.get("/messages/17/")
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Access token is missing"}
+
+# Tests get user info (me) for the unauthenticated
+def test_get_user_unauth():
+    response = client.get("/auth/me/")
+    assert response.status_code == 401
+    assert response.json() == {"detail": "Access token is missing"}
